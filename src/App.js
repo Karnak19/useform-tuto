d2068d2610b388c5a43ba91870794e4b19440fd4
Karnak19 <basile64.v@gmail.com>
2019-11-15 09:37:45 +0100

    adding handleChanges methods to control the inputs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = e => setEmail(e.target.value);
+
+  const handleChangePassword = e => setPassword(e.target.value);
+
   return (
     <div className="App">
       <Form>
         <FormGroup>
           <Label for="email">Email</Label>
-          <Input type="email" name="email" id="email" placeholder="email" />
+          <Input
+            value={email}
+            onChange={handleChangeEmail}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="email"
+          />
         </FormGroup>
         <FormGroup>
           <Label for="password">Password</Label>
           <Input
+            value={password}
+            onChange={handleChangePassword}
             type="password"
             name="password"
             id="password"
