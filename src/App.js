c5d038bc9eb188d807171e86db3a74250fbea93d
Karnak19 <basile64.v@gmail.com>
2019-11-15 09:33:58 +0100

    adding simple Reactstrap form template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Form>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input type="email" name="email" id="email" placeholder="email" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+          />
+        </FormGroup>
+
+        <Button>Submit</Button>
+      </Form>
+    </div>
+  );
 }
 
 export default App;
